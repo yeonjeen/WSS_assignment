6a930f568f52c2365b27acab8b6f723dfafb89b1
yeonjeen <jeendev2019@gmail.com>
2024-01-03 03:34:09 +0900

    feat : User data 기반 FollwersAdapter 구현

--- WssElixir/app/src/main/java/com/example/wsselixir/adapter/FollowersAdapter.kt
+++ WssElixir/app/src/main/java/com/example/wsselixir/adapter/FollowersAdapter.kt
@@ -7,51 +7,52 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.wsselixir.data.FollowerInformation
 import com.example.wsselixir.databinding.ItemFollowerBinding
+import com.example.wsselixir.remote.UserResponseDto
 
-class FollowersAdapter(private var followers: List<FollowerInformation>) :
-    RecyclerView.Adapter<FollowersAdapter.FollowerViewHolder>() {
+class FollowersAdapter(private var users: List<UserResponseDto.User>) :
+    RecyclerView.Adapter<FollowersAdapter.userViewHolder>() {
 
     private lateinit var onItemClickListener: OnItemClickListener
 
     interface OnItemClickListener {
-        fun onItemClick(follower: FollowerInformation.Followers)
+        fun onItemClick(user: UserResponseDto.User)
     }
 
     fun setOnItemClickListener(listener: OnItemClickListener) {
         this.onItemClickListener = listener
     }
 
-    fun setFollowerList(followerList: List<FollowerInformation>) {
-        followers = followerList
+    fun setUsers(userList: List<UserResponseDto.User>) {
+        users = userList
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): userViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemFollowerBinding.inflate(inflater, parent, false)
-        return FollowerViewHolder(binding)
+        return userViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
-        val follower = followers[position]
-        holder.bind(follower as FollowerInformation.Followers)
+    override fun onBindViewHolder(holder: userViewHolder, position: Int) {
+        val user = users[position]
+        holder.bind(user)
     }
 
     override fun getItemCount(): Int {
-        return followers.size
+        return users.size
     }
 
-    inner class FollowerViewHolder(private val binding: ItemFollowerBinding) :
+    inner class userViewHolder(private val binding: ItemFollowerBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private val followerImage: ImageView = binding.imgFollower
 
-        fun bind(follower: FollowerInformation.Followers) {
+        fun bind(user: UserResponseDto.User) {
             Glide.with(itemView)
-                .load(follower.followerImage)
+                .load(user.avatar)
                 .into(followerImage)
 
             itemView.setOnClickListener {
-                onItemClickListener.onItemClick(follower)
+                onItemClickListener.onItemClick(user)
             }
         }
 
